87645d9b183aac3dfea6d97c6ba7168c7f3dcbaf
Marc Grimpo <marc.grimpo@posteo.de>
2019-08-08 14:43:59 +0200

    Let skillsharing_server persist data in file

--- chapter21/skillsharing/skillsharing_server.js
+++ chapter21/skillsharing/skillsharing_server.js
@@ -1,26 +1,29 @@
-var {createServer} = require("http");
+var { createServer } = require("http");
 var Router = require("./router");
 var ecstatic = require("ecstatic");
 
+
 var router = new Router();
-var defaultHeaders = {"Content-Type": "text/plain"};
+var defaultHeaders = { "Content-Type": "text/plain" };
+
 
 var SkillShareServer = class SkillShareServer {
   constructor(talks) {
     this.talks = talks;
     this.version = 0;
     this.waiting = [];
+    this.restore();
 
-    let fileServer = ecstatic({root: "./public"});
+    let fileServer = ecstatic({ root: "./public" });
     this.server = createServer((request, response) => {
       let resolved = router.resolve(this, request);
       if (resolved) {
         resolved.catch(error => {
           if (error.status != null) return error;
-          return {body: String(error), status: 500};
-        }).then(({body,
-                  status = 200,
-                  headers = defaultHeaders}) => {
+          return { body: String(error), status: 500 };
+        }).then(({ body,
+          status = 200,
+          headers = defaultHeaders }) => {
           response.writeHead(status, headers);
           response.end(body);
         });
@@ -41,10 +44,12 @@ const talkPath = /^\/talks\/([^\/]+)$/;
 
 router.add("GET", talkPath, async (server, title) => {
   if (title in server.talks) {
-    return {body: JSON.stringify(server.talks[title]),
-            headers: {"Content-Type": "application/json"}};
+    return {
+      body: JSON.stringify(server.talks[title]),
+      headers: { "Content-Type": "application/json" }
+    };
   } else {
-    return {status: 404, body: `No talk '${title}' found`};
+    return { status: 404, body: `No talk '${title}' found` };
   }
 });
 
@@ -53,7 +58,7 @@ router.add("DELETE", talkPath, async (server, title) => {
     delete server.talks[title];
     server.updated();
   }
-  return {status: 204};
+  return { status: 204 };
 });
 
 function readStream(stream) {
@@ -66,54 +71,58 @@ function readStream(stream) {
 }
 
 router.add("PUT", talkPath,
-           async (server, title, request) => {
-  let requestBody = await readStream(request);
-  let talk;
-  try { talk = JSON.parse(requestBody); }
-  catch (_) { return {status: 400, body: "Invalid JSON"}; }
+  async (server, title, request) => {
+    let requestBody = await readStream(request);
+    let talk;
+    try { talk = JSON.parse(requestBody); }
+    catch (_) { return { status: 400, body: "Invalid JSON" }; }
 
-  if (!talk ||
+    if (!talk ||
       typeof talk.presenter != "string" ||
       typeof talk.summary != "string") {
-    return {status: 400, body: "Bad talk data"};
-  }
-  server.talks[title] = {title,
-                         presenter: talk.presenter,
-                         summary: talk.summary,
-                         comments: []};
-  server.updated();
-  return {status: 204};
-});
+      return { status: 400, body: "Bad talk data" };
+    }
+    server.talks[title] = {
+      title,
+      presenter: talk.presenter,
+      summary: talk.summary,
+      comments: []
+    };
+    server.updated();
+    return { status: 204 };
+  });
 
 router.add("POST", /^\/talks\/([^\/]+)\/comments$/,
-           async (server, title, request) => {
-  let requestBody = await readStream(request);
-  let comment;
-  try { comment = JSON.parse(requestBody); }
-  catch (_) { return {status: 400, body: "Invalid JSON"}; }
+  async (server, title, request) => {
+    let requestBody = await readStream(request);
+    let comment;
+    try { comment = JSON.parse(requestBody); }
+    catch (_) { return { status: 400, body: "Invalid JSON" }; }
 
-  if (!comment ||
+    if (!comment ||
       typeof comment.author != "string" ||
       typeof comment.message != "string") {
-    return {status: 400, body: "Bad comment data"};
-  } else if (title in server.talks) {
-    server.talks[title].comments.push(comment);
-    server.updated();
-    return {status: 204};
-  } else {
-    return {status: 404, body: `No talk '${title}' found`};
-  }
-});
+      return { status: 400, body: "Bad comment data" };
+    } else if (title in server.talks) {
+      server.talks[title].comments.push(comment);
+      server.updated();
+      return { status: 204 };
+    } else {
+      return { status: 404, body: `No talk '${title}' found` };
+    }
+  });
 
-SkillShareServer.prototype.talkResponse = function() {
+SkillShareServer.prototype.talkResponse = function () {
   let talks = [];
   for (let title of Object.keys(this.talks)) {
     talks.push(this.talks[title]);
   }
   return {
     body: JSON.stringify(talks),
-    headers: {"Content-Type": "application/json",
-              "ETag": `"${this.version}"`}
+    headers: {
+      "Content-Type": "application/json",
+      "ETag": `"${this.version}"`
+    }
   };
 };
 
@@ -123,28 +132,59 @@ router.add("GET", /^\/talks$/, async (server, request) => {
   if (!tag || tag[1] != server.version) {
     return server.talkResponse();
   } else if (!wait) {
-    return {status: 304};
+    return { status: 304 };
   } else {
     return server.waitForChanges(Number(wait[1]));
   }
 });
 
-SkillShareServer.prototype.waitForChanges = function(time) {
+SkillShareServer.prototype.waitForChanges = function (time) {
   return new Promise(resolve => {
     this.waiting.push(resolve);
     setTimeout(() => {
       if (!this.waiting.includes(resolve)) return;
       this.waiting = this.waiting.filter(r => r != resolve);
-      resolve({status: 304});
+      resolve({ status: 304 });
     }, time * 1000);
   });
 };
 
-SkillShareServer.prototype.updated = function() {
+SkillShareServer.prototype.updated = function () {
   this.version++;
   let response = this.talkResponse();
   this.waiting.forEach(resolve => resolve(response));
   this.waiting = [];
+  this.persist();
 };
 
+
+const fs = require('fs');
+
+// Nota bene: arrow functions bind the 'this' of the *surrounding context*
+// so you have to use the traditional function() expression here
+SkillShareServer.prototype.persist = async function() {
+  return fs.writeFile(
+    this.backupFile,
+    JSON.stringify({
+      talks: this.talks,
+      version: this.version
+    }),
+    console.log
+  );
+};
+
+SkillShareServer.prototype.restore = function() {
+  try {
+    let buffer = fs.readFileSync(this.backupFile); 
+    let {talks, version} = JSON.parse(buffer);
+    this.talks = talks;
+    this.version = version;
+  }
+  catch(error) {
+    if (error.code == "ENOENT") console.log("server could not restore")
+    else throw error;
+  }
+}
+
+SkillShareServer.prototype.backupFile = "serverBackup";
 new SkillShareServer(Object.create(null)).start(8000);
